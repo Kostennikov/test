796ed3c8802e44238eca4b21d7ae9f09a6c02292
Kostennikov <pavel.kostennikov@gmail.com>
2020-06-22 11:48:04 +0300

    new

--- common.js
+++ common.js
@@ -2,3 +2,9 @@ const DateTime = luxon.DateTime,
 elem = document.querySelector("#data");
 elem.innerText = DateTime.local().setLocale('en-GB').toLocaleString();
 console.log( DateTime.local().setLocale().startOf('week').toISO() );
+
+axios.get('https://api.github.com/users/mzabriskie')
+.then(function (user) {
+  document.getElementById('useravatar').src = user.data.avatar_url;
+  document.getElementById('username').innerHTML = user.data.name;
+});
--- dist/test.e31bb0bc.js
+++ dist/test.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56882" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50313" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
